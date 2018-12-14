b3e71a0a775a8c265015108e641d1578254056c8
Ryna <pinktulips@Hotmail.com>
2018-12-14 10:53:06 -0800

    reg ex

--- code-challenges/301/reg_ex/challenege-14.test.js
+++ code-challenges/301/reg_ex/challenege-14.test.js
@@ -7,7 +7,10 @@ For example, ['apple', 'banana', 'MacGyver'] returns ['Apple', 'Banana', 'MacGyv
 ------------------------------------------------------------------------------------------------ */
 
 const toTitleCase = (arr) => {
-  // Solution code here...
+  for(var i = 0; i < arr.length; i++){
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
